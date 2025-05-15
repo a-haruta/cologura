d3471e63631f0c995aefa70b93991f453f03669e
a-haruta <a-haruta@colopl.co.jp>
2025-05-15 22:14:08 +0900

    WebGPU情報を取得する機能を追加し、HTMLを更新

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-export function add(a: number, b: number): number {
-  return a + b;
+
+export async function getWebGPUInfo(): Promise<string>
+{
+    const adapter = await navigator.gpu.requestAdapter();
+    const c = adapter?.propertyIsEnumerable("info")
+    return `
+${adapter?.info.device}
+${adapter?.info.architecture}
+${adapter?.info.vendor}
+${adapter?.info.description}
+${Array.from(adapter?.features ?? []).join('\n')}
+    `;
 }
